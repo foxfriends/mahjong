89687c6306eaa3928068c5053b5f2f403539b975
Cameron Eldridge <cameldridge@gmail.com>
2020-04-16 13:47:09 -0400

    Add a 3D table

--- client/src/app/App.svelte
+++ client/src/app/App.svelte
@@ -1,7 +1,25 @@
 <script>
+  import Table from './Table.svelte';
+
   export let socket;
+
+  let name, room;
 </script>
 
-<div>
-  Hello world
+<div class="layer">
+  <Table>
+    Hello world
+  </Table>
 </div>
+<div class="layer">
+</div>
+
+<style>
+.layer {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ client/src/app/Table.svelte
@@ -0,0 +1,67 @@
+<div class="world">
+  <div class="table">
+    <div class="top-edge" />
+    <div class="left-edge" />
+    <div class="right-edge" />
+    <div class="bottom-edge" />
+    <slot />
+  </div>
+</div>
+
+<style>
+.world {
+  position: relative;
+  perspective: 20in;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.table {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  transform: rotateX(50deg);
+  background-image: var(--image-table);
+  transform-style: preserve-3d;
+}
+
+.top-edge, .bottom-edge {
+  position: absolute;
+  left: 0;
+  width: 100%;
+  height: 0.5cm;
+  background-color: #2c4730;
+}
+
+.top-edge {
+  bottom: 100%;
+  transform-origin: bottom;
+  transform: rotateX(-90deg);
+}
+
+.bottom-edge {
+  top: 100%;
+  transform-origin: top;
+  transform: rotateX(90deg);
+}
+
+.left-edge, .right-edge {
+  position: absolute;
+  top: 0;
+  width: 0.5cm;
+  height: 100%;
+  background-color: #264231;
+}
+
+.left-edge {
+  right: 100%;
+  transform-origin: right;
+  transform: rotateY(90deg);
+}
+.right-edge {
+  left: 100%;
+  transform-origin: left;
+  transform: rotateY(-90deg);
+}
+</style>
