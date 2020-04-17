ed76cca6c9f0e79ec56c494e5e084e25715154bf
Cameron Eldridge <cameldridge+git@gmail.com>
2020-04-17 16:55:09 -0400

    Ready button UI

--- client/src/app/Status/ReadyButton.svelte
+++ client/src/app/Status/ReadyButton.svelte
@@ -14,10 +14,29 @@
   }
 </script>
 
-<button on:click={toggleReady}>
+<button class='button' on:click={toggleReady}>
   {#if $store[$store.playerWind(socket.name)].ready}
     Cancel
   {:else}
     Ready
   {/if}
 </button>
+
+<style>
+  .button {
+    position: absolute;
+    left: 50%;
+    bottom: 32px;
+    width: 300px;
+    height: 50px;
+    transform: translateX(-50%);
+
+    border: 1px solid rgba(255, 255, 255, 0.25);
+    border-radius: 28px;
+    background: rgba(255, 255, 255, 0.12);
+    font-family: var(--font-english);
+    font-size: 16pt;
+    color: white;
+    cursor: pointer;
+  }
+</style>
